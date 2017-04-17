f4c80d277db82b8ff76055142b45214ab49b6a8d
Pete Deevakul <editor@mozzarella.website>
2017-04-17 17:19:14 -0400

    resize plot text

--- R/education-eda.R
+++ R/education-eda.R
@@ -48,6 +48,8 @@ str(edu)
 library(rpart)
 library(rpart.plot)
 library(tree)
+library(ggtree)
+library(ggdendro)
 
 # remove non-numeric variables
 edu.tree <- edu %>%
@@ -66,13 +68,14 @@ show.prp.palettes()
 # can be experimented with - perhaps on a dataframe with more variables.
 edu.rpart01 <- rpart(perCapitaFFL ~ ., data = edu.tree, 
                      control = rpart.control())
+
 print(edu.rpart01)
 summary(edu.rpart01)
 
 # plot tree - 4 digits
 rpart.plot(edu.rpart01, 
            type = 1, extra = 1, digits = 4, cex = 0.8, 
-           branch.lty = 3, branch.lwd = 2.5,
+           branch.lty = 1, branch.lwd = 1,
            split.cex = 1.1, nn.cex = 0.9,
            box.palette = "BuBn", 
            pal.thresh = mean(edu$perCapitaFFL),
@@ -80,13 +83,30 @@ rpart.plot(edu.rpart01,
            family = "GillSans")
 
 prp(edu.rpart01, 
-    type = 4, extra = 1, digits = 4, under = T,
-    branch.lty = 2, branch.lwd = 2.5, 
-    cex = 0.8,  split.cex = 1.1, nn.cex = 0.9,
+    type = 1, extra = 1, digits = 4, under = F,
+    branch.lty = 1, branch.lwd = 1, 
+    cex = 1,  split.cex = 1.1, nn.cex = 1,
     box.palette = "BuBn",
     family = "GillSans")
 
 rsq.rpart(edu.rpart01)
+
+# rpart ggdendro 
+rp01 <- dendro_data(edu.rpart01, type = "triangle")
+str(rp01)
+
+ggplot(segment(rp01)) + 
+  geom_segment(aes(x = x, y = y, xend = xend, yend = yend)) + 
+  geom_text(aes(x = x, y = y, label = label), 
+            size = 4, vjust = 1.75, hjust = 1.05,
+            data = label(rp01)) +
+  geom_text(data = rp01$leaf_labels, 
+            aes(x = x, y = y, label = label), 
+            size = 4, vjust = 0.5, hjust = -0.25) +
+  geom_
+  coord_flip() + 
+  scale_y_reverse(expand = c(0.5, 0)) +
+  theme_dendro()
 
 # `tree` regression tree 01 --------------------------------------------------
 
