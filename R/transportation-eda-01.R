b3868a41f26f45212bb7ffd2b0992c31b53e8918
Pete Deevakul <editor@mozzarella.website>
2017-04-21 03:37:19 -0400

    create bar plots on total means-of-transportation data

---
+++ R/transportation-eda-01.R
@@ -0,0 +1,123 @@
+# ATF - Federal Firearms Licenses
+# US Census - American Community Survey - Table S0802
+# "MEANS OF TRANSPORTATION TO WORK BY SELECTED CHARACTERISTICS"
+# https://www.census.gov/acs/www/data/data-tables-and-tools/subject-tables/
+
+# There are 3 means of transportation across multiple characteristics.
+# 1. Car, truck, or van - drove alone
+# 2. Car, truck, or van - carpooled
+# 3. Public transportation (excluding taxicabs)
+
+# load data -------------------------------------------------------------------
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+# vis themes
+source("R/00-pd-themes.R")
+
+# means of transportation by industry
+industry <- read.csv("data/03-additional-data/transporation-by-industry.csv")
+str(industry)
+summary(industry)
+
+# departure time from home to work
+departure.time <- read.csv("data/03-additional-data/transportation-time-leaving-home.csv")
+str(departure.time)
+summary(departure.time)
+
+# Federal Firearms Licenses
+ffl <- read.csv("data/ffl-per-capita.csv")
+
+# compare total population to total surveyed population
+sum(ffl$POPESTIMATE2015)    # 320,226,241
+sum(industry$total.workers) # 147,966,010
+
+sum(ffl$POPESTIMATE2015) - sum(industry$total.workers) 
+# There is a difference of 172,260,231
+
+# explore: totals -------------------------------------------------------------
+
+# select totals, bind firearms data, and compute per capita figures
+industry.perCapita <- industry %>%
+  select(NAME, 
+         total.workers, 
+         total.drove.alone, 
+         total.carpool, 
+         total.public.transportation) %>%
+  left_join(ffl, by = "NAME") %>%
+  mutate(percent.working = total.workers/POPESTIMATE2015,
+         perCapita.workers = (total.workers/POPESTIMATE2015) * 100000,
+         perCapita.drove.alone = (total.drove.alone/POPESTIMATE2015) * 100000,
+         perCapita.carpool = (total.carpool/POPESTIMATE2015) * 100000,
+         perCapita.public = (total.public.transportation/POPESTIMATE2015) * 100000) %>%
+  select(-POPESTIMATE2016)
+
+# rename variables for cleaner plots
+colnames(industry.perCapita)[2:5] <- gsub("total.", "", colnames(industry.perCapita)[2:5])
+colnames(industry.perCapita)[9:12] <- c("workers.per.capita", "a.drove.alone", "b.carpool",
+                                        "c.public.transportation")
+
+# stack data and facet vis - total data
+industry.perCapita %>%
+  select(NAME, perCapitaFFL, workers, 
+         drove.alone, carpool, public.transportation) %>%
+  gather(key = means, value = total, 3:6) %>%
+  ggplot(aes(reorder(NAME, total), 
+             total, 
+             fill = total)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~ means, nrow = 3) +
+  pd.facet + 
+  theme(axis.text.x = element_text(size = 11),
+        legend.position = "right") +
+  coord_flip() +
+  labs(x = "", y = "")
+
+# stack data and facet vis - per capita data
+industry.perCapita %>%
+  select(NAME, perCapitaFFL, 
+         workers.per.capita, 
+         a.drove.alone, b.carpool, c.public.transportation) %>%
+  gather(key = means, value = total, 3:6) %>%
+  filter(means != "workers.per.capita") %>%
+  ggplot(aes(reorder(NAME, total), 
+             total, 
+             fill = total)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~ means, ncol = 1) +
+  pd.facet + 
+  theme(axis.text.x = element_text(size = 11),
+        axis.text.y = element_text(size = 8)) +
+  coord_flip() +
+  labs(x = "", y = "")
+
+# stack data and stacked vis - per capita data
+industry.pc.stack <- industry.perCapita %>%
+  select(NAME, perCapitaFFL, workers.per.capita, 
+         a.drove.alone, b.carpool, c.public.transportation) %>%
+  gather(key = means, value = total, 3:6) %>%
+  filter(means != "workers.per.capita") %>%
+  arrange(total)
+
+# stacked bar chart
+ggplot(industry.pc.stack, aes(reorder(NAME, total), 
+             total, 
+             fill = means)) +
+  geom_bar(stat = "identity", position = "stack") +
+  scale_fill_manual(values = c("deepskyblue4", 
+                               "firebrick4", 
+                               "antiquewhite2")) +
+  pd.theme + 
+  coord_flip() +
+  guides(fill = guide_legend(reverse = F)) +
+  labs(x = "", y = "", fill = "transporation")
+
+
+
+
+
+
+
+
